44ed9194ff4ca428d910e233273ab94097a78abf
mjcampagna <mjcampagna@gmail.com>
2018-07-22 11:21:43 -0700

    column-toggle checkboxes now bound to component state

--- src/components/pangolin/Pangolin.jsx
+++ src/components/pangolin/Pangolin.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 
 // import Pangolin from '../../js/pangolin.js';
 
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
 import Header from './Header.jsx';
 import Main from './Main.jsx';
 import Footer from './Footer.jsx';
@@ -13,18 +28,47 @@ export default class Layout extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.state = {
+			columnL: false,
+			columnR: false
+		}
+
 	}
 
 	componentDidMount() {
-		// const pangolin = new Pangolin();
+
+		window.addEventListener( 'resize', debounce( function() {
+			console.log('Resize!')
+		}, 250 ));
+
+		window.addEventListener('scroll', function(e) {
+			if ( window.scrollY > 0 ) {
+				document.body.classList.add('is-scrolling');
+			} else {
+				document.body.classList.remove('is-scrolling');
+			}		
+		}, false);
+
+	}
+
+	handleChangeOnToggleColumn(event) {
+		this.setState({
+			[event.target.id]: event.target.checked
+		})
 	}
 
 	render() {
 		return (
 			<React.Fragment>
 				<div className="page" data-layout="3col split">
-					<input type="checkbox" className="toggleColumn" id="columnL" />
-					<input type="checkbox" className="toggleColumn" id="columnR" />
+					<input type="checkbox" className="toggleColumn" id="columnL" 
+						checked={this.state.columnL} 
+						onChange={(e) => this.handleChangeOnToggleColumn(e)} 
+					/>
+					<input type="checkbox" className="toggleColumn" id="columnR" 
+						checked={this.state.columnR} 
+						onChange={(e) => this.handleChangeOnToggleColumn(e)} 
+					/>
 		
 					<div className="pageBody">
 						<Header />
