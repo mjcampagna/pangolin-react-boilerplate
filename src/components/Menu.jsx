21d8aac56212ef37524ef64861b96df83b8f9d04
mjcampagna <mjcampagna@gmail.com>
2018-07-28 22:23:46 -0700

    Clean up leftover code

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Menu(props) {
-
-	const dispatch = action => {
-		props.dispatch( action );
-	};
-
 	return (
 		<ul className={props.classes}>
 			<li className="menu-item">
